3e44c9a25f67a17c9a440f418ed7fae1e7c15aa8
jirka-kruml <jirka.kruml@gmail.com>
2018-05-09 20:18:20 +0200

    Add upcoming events for logged user

--- app/src/main/java/cz/muni/fi/pv239/teamup/activities/UpcomingEventsActivity.kt
+++ app/src/main/java/cz/muni/fi/pv239/teamup/activities/UpcomingEventsActivity.kt
@@ -1,15 +1,13 @@
 package cz.muni.fi.pv239.teamup.activities
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
-import com.firebase.ui.auth.AuthUI
 import com.google.firebase.FirebaseApp
 import com.google.firebase.database.*
 import cz.muni.fi.pv239.teamup.R
@@ -19,7 +17,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 
-class MainActivity : AppCompatActivity() {
+class UpcomingEventsActivity : AppCompatActivity() {
 
     // database
     private lateinit var database: DatabaseReference
@@ -35,15 +33,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_upcoming_events)
 
         // Initialize Firebase
         FirebaseApp.initializeApp(this)
         database = FirebaseDatabase.getInstance().reference
-
-        fabMainView.setOnClickListener { view ->
-            run { startActivity(Intent(this, AddEventActivity::class.java)) }
-        }
 
         // register recycler view
         listAdapter = RecyclerViewAdapter(events, { event, view ->
@@ -81,7 +75,14 @@ class MainActivity : AppCompatActivity() {
             override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
                 val event: SportEvent = dataSnapshot.getValue(SportEvent::class.java)
                         ?: throw IllegalStateException("Added Event is null")
-                if (SportEvent.dateFormatter.parse(event.date).after(Calendar.getInstance().time)) {
+
+
+
+                val shpr = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE)
+                val uid = shpr.getString("user.uid", null)
+                if (SportEvent.dateFormatter.parse(event.date).after(Calendar.getInstance().time)
+                        && event.signedUsers.contains(uid)) {
+
                     events[dataSnapshot.key] = event
                     listAdapter.notifyDataSetChanged()
                 }
@@ -90,43 +91,4 @@ class MainActivity : AppCompatActivity() {
         database.child("events").addChildEventListener(childEventListener)
 
     }
-
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.getItemId()) {
-            R.id.action_upcoming_events -> {
-                startActivity(Intent(this, UpcomingEventsActivity::class.java))
-                return true
-            }
-            R.id.action_my_events -> {
-                startActivity(Intent(this, MyEventsActivity::class.java))
-                return true
-            }
-            R.id.action_history -> {
-                startActivity(Intent(this, HistoryActivity::class.java))
-                return true
-            }
-            R.id.action_signout -> {
-                signOut()
-                return true
-            }
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
-
-    private fun signOut() {
-        AuthUI.getInstance().signOut(this)
-        startActivity(Intent(this, SignInActivity::class.java))
-    }
-
-    override fun onBackPressed() {
-        // do nothing - we don't want to return to sign in page
-    }
-
 }
